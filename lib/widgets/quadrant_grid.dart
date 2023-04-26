7e2ce3ba89f4971258aac0f23e67fece3f868981
Su Guoyu <yss2813483011xxl@gmail.com>
2023-04-26 23:34:39 +0800

    Add files via upload
    Support browsing and archiving of tasks on quadrant cards

--- lib/widgets/quadrant_grid.dart
+++ lib/widgets/quadrant_grid.dart
@@ -1,60 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:quadrant_master/models/task.dart';
+import 'package:quadrant_master/providers/tasks_provider.dart';
+import 'package:provider/provider.dart';
 
 class QuadrantGrid extends StatelessWidget {
   final Function(int) onTap;
 
   QuadrantGrid({required this.onTap});
 
+  Widget _buildTaskList(
+      BuildContext context, int quadrant, List<Task> tasks, TasksProvider tasksProvider) {
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Checkbox(
+            value: tasks[index].isCompleted,
+            onChanged: (newValue) {
+              tasksProvider.toggleTaskCompletion(tasks[index].id, newValue!);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('任务已归档'),
+                  action: SnackBarAction(
+                    label: '撤销',
+                    onPressed: () {
+                      tasksProvider.toggleTaskCompletion(tasks[index].id, !newValue);
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+          title: Text(tasks[index].title),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: List.generate(4, (index) {
-        return InkWell(
-          onTap: () => onTap(index + 1),
-          child: Container(
-            margin: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12.0),
-              color: Colors.blue.shade200,
-            ),
-            child: Center(
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: index == 0
-                          ? '重要'
-                          : index == 1
-                              ? '重要'
-                              : index == 2
+    final tasksProvider = Provider.of<TasksProvider>(context);
+    return Scaffold(
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(4, (index) {
+          final quadrantTasks = tasksProvider.getUnarchivedTasksByQuadrant(index + 1);
+          return InkWell(
+            onTap: () => onTap(index + 1),
+            child: Container(
+              margin: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.blue.shade200,
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: index == 0
+                                  ? '重要'
+                                  : index == 1
+                                  ? '重要'
+                                  : index == 2
                                   ? '紧急'
                                   : '不重要',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: '\n',
-                    ),
-                    TextSpan(
-                      text: index == 0
-                          ? '且紧急'
-                          : index == 1
-                              ? '不紧急'
-                              : index == 2
+                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text: '\n',
+                            ),
+                            TextSpan(
+                              text: index == 0
+                                  ? '且紧急'
+                                  : index == 1
+                                  ? '不紧急'
+                                  : index == 2
                                   ? '不重要'
                                   : '且紧急',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ],
-                ),
-                textAlign: TextAlign.center,
+                  ),
+                  quadrantTasks.isNotEmpty
+                      ? Expanded(
+                    flex: 2,
+                    child: _buildTaskList(context, index + 1, quadrantTasks, tasksProvider),
+                  )
+                      : SizedBox(),
+                ],
               ),
             ),
-          ),
-        );
-      }),
+          );
+        }),
+      ),
     );
   }
 }

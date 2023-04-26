7d0e2b2751557df69c974fbdd87f80bb4bf9b419
Su Guoyu <yss2813483011xxl@gmail.com>
2023-04-26 23:16:49 +0800

    Add files via upload
    Slightly modified the style of the four quadrant cards on the home page

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:provider/provider.dart';
 import 'package:quadrant_master/providers/tasks_provider.dart';
 import 'package:quadrant_master/screens/home_screen.dart';
 
-  // 将 main 函数更改为异步，并等待 tasksProvider.loadTasks() 完成
+// 将 main 函数更改为异步，并等待 tasksProvider.loadTasks() 完成
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // 在使用异步方法前确保Flutter绑定初始化
   final tasksProvider = TasksProvider();
   await tasksProvider.loadTasks(); // 加载任务列表
 
   runApp(
-  // 将 ChangeNotifierProvider 移到了 runApp 函数内，这样你可以在加载任务列表后将 tasksProvider 传递给它
+    // 将 ChangeNotifierProvider 移到了 runApp 函数内，这样你可以在加载任务列表后将 tasksProvider 传递给它
     ChangeNotifierProvider(
       create: (context) => tasksProvider,
       child: MyApp(),
--- lib/widgets/quadrant_grid.dart
+++ lib/widgets/quadrant_grid.dart
@@ -1,3 +1,42 @@
+// import 'package:flutter/material.dart';
+//
+// class QuadrantGrid extends StatelessWidget {
+//   final Function(int) onTap;
+//
+//   QuadrantGrid({required this.onTap});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.count(
+//       crossAxisCount: 2,
+//       children: List.generate(4, (index) {
+//         return InkWell(
+//           onTap: () => onTap(index + 1),
+//           child: Container(
+//             margin: const EdgeInsets.all(8.0),
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(12.0),
+//               color: Colors.blue.shade200,
+//             ),
+//             child: Center(
+//               child: Text(
+//                 // 修改象限标题
+//                 index == 0
+//                     ? '重要且紧急'
+//                     : index == 1
+//                     ? '重要不紧急'
+//                     : index == 2
+//                     ? '紧急不重要'
+//                     : '不重要且不紧急',
+//                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//               ),
+//             ),
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class QuadrantGrid extends StatelessWidget {
@@ -19,9 +58,35 @@ class QuadrantGrid extends StatelessWidget {
               color: Colors.blue.shade200,
             ),
             child: Center(
-              child: Text(
-                '象限 ${index + 1}',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: index == 0
+                          ? '重要'
+                          : index == 1
+                          ? '重要'
+                          : index == 2
+                          ? '紧急'
+                          : '不重要',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: '\n',
+                    ),
+                    TextSpan(
+                      text: index == 0
+                          ? '且紧急'
+                          : index == 1
+                          ? '不紧急'
+                          : index == 2
+                          ? '不重要'
+                          : '且紧急',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
           ),

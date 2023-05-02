89ee82920ca83200bf669cdc2700e9ad42b26346
Su Guoyu <yss2813483011xxl@gmail.com>
2023-05-02 22:55:07 +0800

    Add files via upload
    Added zoom animation for opening and closing quadrants

--- lib/screens/quadrant_detail_screen.dart
+++ lib/screens/quadrant_detail_screen.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:quadrant_master/models/task.dart';
 import 'package:quadrant_master/providers/tasks_provider.dart';
-import 'package:quadrant_master/screens/edit_task_screen.dart';
 import 'package:quadrant_master/widgets/task_list.dart';
+import 'package:provider/provider.dart';
 
 class QuadrantDetailScreen extends StatelessWidget {
   final int quadrant;
 
-  QuadrantDetailScreen({required this.quadrant});
+  const QuadrantDetailScreen({required this.quadrant, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final tasksProvider = Provider.of<TasksProvider>(context);
-    final List<Task> tasks = tasksProvider.getUnarchivedTasksByQuadrant(quadrant);
+    final List<Task> quadrantTasks = tasksProvider.getTasksByQuadrant(quadrant);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('象限 $quadrant'),
       ),
-      body: TaskList(tasks: tasks),
+      body: Hero(
+        tag: 'quadrant-$quadrant',
+        child: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: Material(
+            type: MaterialType.transparency,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.blue.shade200,
+              ),
+              child: TaskList(tasks: quadrantTasks),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

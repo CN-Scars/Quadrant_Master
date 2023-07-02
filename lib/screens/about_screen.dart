361e3c596d4f0c2065e24ead89a6251b98361ec1
CN-Scars <yss2813483011xxl@gmail.com>
2023-07-02 15:05:03 +0800

    Revised "About" page
    Added "Support Developer" card for donating to developer

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -5,13 +5,23 @@ import 'package:url_launcher/url_launcher.dart';
 class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 打开Github主页的方法
     _launchGithubURL() async {
-      // 打开Github主页的方法
       const githubUrl = 'https://github.com/CN-Scars';
       if (await canLaunch(githubUrl)) {
         await launch(githubUrl);
       } else {
         launch(githubUrl);
+      }
+    }
+
+    // 打开Patreon主页的方法
+    _launchPatreonURL () async {
+      const patreonUrl = 'https://www.patreon.com/Scars138';
+      if (await canLaunch(patreonUrl)) {
+        await launch(patreonUrl);
+      } else {
+        launch(patreonUrl);
       }
     }
 
@@ -28,6 +38,15 @@ class AboutScreen extends StatelessWidget {
               subtitle: Text('开发者'),
               title: Text('Scars'),
               onTap: _launchGithubURL,
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(16.0),
+            child: ListTile(
+              leading: Icon(MdiIcons.patreon),
+              subtitle: Text('支持开发者'),
+              title: Text('Scars'),
+              onTap: _launchPatreonURL,
             ),
           ),
         ],

486915fb15d40a88610ba6100bb4312556cf8428
CN-Scars <yss2813483011xxl@gmail.com>
2023-07-16 16:30:40 +0800

    Create a Task entity class

--- quadrant_master_backend/src/main/java/com/scars/quadrant_master_backend/model/Task.java
+++ quadrant_master_backend/src/main/java/com/scars/quadrant_master_backend/model/Task.java
@@ -1,4 +1,63 @@
 package com.scars.quadrant_master_backend.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private int quadrant;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    //Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuadrant() {
+        return quadrant;
+    }
+
+    public void setQuadrant(int quadrant) {
+        this.quadrant = quadrant;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
